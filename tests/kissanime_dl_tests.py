c99b8d0140d69f18e2c0b4abca4150cb80fc53c7
abdullah2993 <a.saleem2993@gmail.com>
2017-02-02 12:46:24 +0500

    Travis

--- tests/kissanime_dl_tests.py
+++ tests/kissanime_dl_tests.py
@@ -16,7 +16,7 @@ class KissAnimeTests(unittest.TestCase):
         title1 = "Naruto Shippuuden (Sub) Episode 112"
         title2 = "Naruto Shippuuden (Sub) Episode 086-087"
         self.assertEqual(KissAnime._get_ep_no(title1), ["112"])
-        self.assertEqual(KissAnime._get_ep_no(title2), ["086","087"])
+        self.assertEqual(KissAnime._get_ep_no(title2), ["86","87"])
 
 if __name__ == '__main__':
     unittest.main()
