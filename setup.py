5bd2e1eac2f9c45e3a59564c4087d3d2ec5302bc
abdullah2993 <a.saleem2993@gmail.com>
2017-02-02 12:16:44 +0500

    some more changes

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ from distutils.core import setup
 setup(
     name='KissAnime Downloader',
     version='0.1',
-    py_modules=['kissanime_dl'],
+    scripts=['kissanime_dl.py'],
     description='Download anime episodes from KissAnime',
     author='Abdullah Saleem',
     author_email='a.saleem2993@gmail.com',
     url='https://github.com/Abdullah2993/KissAnime-Downloader',
-    license='MIT'
+    license='MIT',
+    keywords=['kissanime', 'download', 'downloader', 'anime', 'video']
 )
